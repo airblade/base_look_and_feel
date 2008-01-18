2e88f126a69e5cf2c6c02ea7d290ee19f1c90510
Andy Stewart <boss@airbladesoftware.com>
2008-01-18 18:48:57 +0000

    Added icons for Wufoo style form buttons/links and form error feedback.  Improved README.

--- generators/base_look_and_feel/base_look_and_feel_generator.rb
+++ generators/base_look_and_feel/base_look_and_feel_generator.rb
@@ -6,6 +6,11 @@ class BaseLookAndFeelGenerator < Rails::Generator::Base
 
       m.directory 'app/views/layouts'
       m.file 'application.html.erb', 'app/views/layouts/application.html.erb', :collision => :ask
+      
+      m.directory 'public/images/icons'
+      %w( arrow_undo cross error exclamation pencil tick ).each do |icon|
+        m.file "#{icon}.png", "public/images/icons/#{icon.png}", :collision => :ask
+      end
     end
   end
 end
